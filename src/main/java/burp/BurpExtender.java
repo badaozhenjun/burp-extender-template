239cea0a440cce38c281dc511caffc7a3d49b19c
396155102@qq.com <396155102>
2020-01-26 16:25:47 +0800

    修复apijar的源码

--- src/main/java/burp/BurpExtender.java
+++ src/main/java/burp/BurpExtender.java
@@ -1,8 +1,27 @@
 package burp;
 
-public class BurpExtender implements IBurpExtender {
+import java.io.PrintWriter;
+
+public class BurpExtender implements IBurpExtender,IHttpListener {
+    private IBurpExtenderCallbacks callbacks;
+    private IExtensionHelpers helpers;
+    private PrintWriter stdout;
     public void registerExtenderCallbacks(IBurpExtenderCallbacks callbacks) {
-        callbacks.setExtensionName("aaaa");
-        callbacks.printOutput("hahahaha");
+        this.callbacks = callbacks;
+        this.helpers = callbacks.getHelpers();
+        callbacks.setExtensionName("MarkINFO");
+        this.stdout = new PrintWriter(callbacks.getStdout(), true);
+        this.callbacks.registerHttpListener(this);
+//        this.callbacks.registerMessageEditorTabFactory(this);
+        this.stdout.print("author: badaozhenjun");
     }
+
+
+    public void processHttpMessage(int toolFlag, boolean messageIsRequest, IHttpRequestResponse messageInfo) {
+
+    }
+
+//    public IMessageEditorTab createNewInstance(IMessageEditorController controller, boolean editable) {
+//        return null;
+//    }
 }
